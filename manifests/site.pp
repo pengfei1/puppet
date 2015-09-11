8dbda3425e844dbc6819dde87ba4ece70ed4f419
pengfei1 <f@dev1.m.com>
2015-09-11 21:17:46 +0800

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,8 @@
 $puppetmaster = hiera('puppetmaster', 'dev1.m.com')
+$puppetrole = $puppetmaster ? {
+    $puppetmaster : 'puppet-master',
+    default       : 'puppet-agent',
+}
 node default{
     hiera_include('classes')
 }
