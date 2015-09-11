e8dfe1683da186b076ce5ebef136c37ba97968c5
pengfei1 <f@dev1.m.com>
2015-09-11 21:13:59 +0800

    test

---
+++ modules/puppet/manifests/agent.pp
@@ -0,0 +1,12 @@
+class puppet::agent {
+    service { 'puppet' :
+        ensure => running,
+        hasrestart => true,
+        require => [File['/etc/puppet/puppet.conf']]
+    }
+    file { '/etc/puppet/puppet.conf':
+        content => template('puppet/puppet-agent.conf.erb'),
+        mode    => '0755',
+    }
+}
+
---
+++ modules/puppet/manifests/master.pp
@@ -0,0 +1,36 @@
+class puppet::master {
+    file { '/usr/local/bin/pull-update':
+        source => 'puppet:///modules/puppet/pull-updates.sh',
+        mode   => '0755',
+    }
+    file { '/etc/puppet/puppet.conf':
+        content => template('puppet/puppet-master.conf.erb'),
+        mode    => '0755',
+    }
+    file { '/etc/puppet/autosign.conf':
+        content => template('puppet/autosign.conf.erb'),
+        mode    => '0755',
+    }
+    cron { 'run-puppet':
+        ensure  => 'present',
+        user    => 'puppet',
+        command => '/usr/local/bin/pull-updates',
+        minute  => '*/10',
+        hour    => '*',
+    }
+    service { 'apache2' :
+        ensure      => running,
+        hasrestart  => true,
+        require     => [File['/etc/puppet/autosign.conf'], File['/etc/puppet/puppet.conf']],
+    }
+    file { '/etc/puppet/hiera.yaml':
+        source => 'puppet:///modules/puppet/hiera.yaml',
+        mode   => '0755',
+    }
+    file { '/etc/hiera.yaml':
+        ensure  => link,
+        target  => '/etc/puppet/hiera.yaml',
+        require => File['/etc/puppet/hiera.yaml'],
+        notify  => Service['apache2']
+    }
+}
