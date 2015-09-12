4c5ff06b1e2e022e7b9dce3009846a05dde1886f
pengfei1 <f@dev1.m.com>
2015-09-12 21:29:45 +0800

    test

--- modules/puppet/facts.d/get_server_type.py
+++ modules/puppet/facts.d/get_server_type.py
@@ -15,12 +15,28 @@ sys.setdefaultencoding('utf-8')
 import socket
 
 #每个机器的角色分配
+pkuph_ks_slave = [
+    's12.pkuph-ks.com',        
+    's13.pkuph-ks.com',        
+    's14.pkuph-ks.com',        
+    's15.pkuph-ks.com',        
+    's16.pkuph-ks.com',        
+    's17.pkuph-ks.com',        
+    's18.pkuph-ks.com',        
+    's19.pkuph-ks.com',        
+    's20.pkuph-ks.com',        
+]
 server_spec = {
     'm.com' : {
         'puppet_master' : ['dev1.m.com'],
         'hbase_thrift' : ['dev1.m.com'],
         'puppet_agent' : ['dev0.m.com']
-    }        
+    },
+    'pkuph-ks.com' : {
+        'puppet_master' : ['s11.pkuph-ks.com'],
+        'hbase_thrift' : ['s11.pkuph-ks.com'],
+        'puppet_agent' : pkuph_ks_slave
+    }
 }
 
 fqdn = socket.gethostname()
