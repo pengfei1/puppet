8534b8f9aad7c61ba7d801e33a4047438b949894
pengfei1 <f@bj3.m.com>
2015-09-15 22:30:40 +0800

    test

--- manifests/puppetdb.pp
+++ manifests/puppetdb.pp
@@ -2,6 +2,9 @@ node default {
     class { 'puppetdb':
         listen_address => "$fqdn",
     }
+    class { 'puppetdb::params':
+        postgres_version => '9.3'
+    }
     class {'puppetdb::master::config':
         puppet_service_name => 'apache2',
     }
