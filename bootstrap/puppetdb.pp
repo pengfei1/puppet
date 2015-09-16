4a0966834588d556e71cf6891b42799955f5e1ee
pengfei1 <f@bj3.m.com>
2015-09-16 16:15:36 +0800

    puppetdb

--- bootstrap/puppetdb.pp
+++ bootstrap/puppetdb.pp
@@ -1,6 +1,9 @@
 class { 'puppetdb':
     listen_address => "$fqdn",
 }
+class { 'puppetdb::globals':
+    version => '2.3.7-1puppetlabs1'
+}
 class {'puppetdb::master::config':
     puppet_service_name => 'apache2',
 }
