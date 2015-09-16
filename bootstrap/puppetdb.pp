ea46c45c04c80728e5710488f27f975edc5aaa14
pengfei1 <f@bj3.m.com>
2015-09-16 19:45:04 +0800

    add user

--- bootstrap/puppetdb.pp
+++ bootstrap/puppetdb.pp
@@ -1,5 +1,5 @@
 class { 'puppetdb':
-    listen_address => "$fqdn",
+    listen_address => "0.0.0.0",
 }
 class {'puppetdb::master::config':
     puppet_service_name => 'apache2',
--- modules/admin/manifests/user.pp
+++ modules/admin/manifests/user.pp
@@ -1,4 +1,34 @@
+define ssh_user($name, $key = node, $keytype = "ssh-rsa", $role = "dev"){
+    user { "$name":
+        ensure => present,
+        shell   => '/bin/bash',
+    }
+    file {["/home/${name}", "/home/${name}/.ssh"]:
+        ensure => directory,
+        mode => '0700',
+        owner => $name,
+        require => User["${name}"]
+    }
+    if $key != nodef {
+        ssh_authorized_key { "${name}_key":
+            key     => $key,
+            type    => "$keytype",
+            user    => $name,
+            require => File["/home/${name}/.ssh"],
+        }
+    }   
+#    if $role == "admin" {
+#        augeas { "${name}_sudo":
+#            context => "/files/etc/sudoers",
+#            changes => "set 
+#        }
+#    }
+
+}
 class admin::user{
-    $admin_user = hiera('admin_user', ['f'])
-    $dev_user = hiera('dev_user', ['f'])
+    $admin_user = hiera('administrator', [])
+    $dev_user = hiera('developer', [])
+    $admin_user.each | Integer $index, String $value | {
+        ssh_user($value)
+    }
 }
